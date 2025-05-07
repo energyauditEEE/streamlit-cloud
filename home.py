b2caad1ec88e88ffd8402637d2dcc3a6baf45edc
energyauditEEE <energyauditEEE2025@gmail.com>
2025-05-08 01:06:18 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from anomaly import show as show_anomaly
 from compare import show as show_compare
-from costcalculator import show as show_costcalculator
 from savingsplan import show as show_savingsplan
 from solar import show as show_solar
 from wind import show as show_wind
+from costcalculator import show as show_costcalculator
 
 def main():
     st.title("Energy and Anomaly Management Dashboard")
@@ -12,10 +12,10 @@ def main():
     # Create tabs for each of your sections
     tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Anomaly Detection",
                                                  "Comparison",
-                                                 "Cost Calculator",
                                                  "Savings Plan",
                                                  "Solar Dashboard",
-                                                 "Wind Dashboard"])
+                                                 "Wind Dashboard",
+                                                 "Cost Calculator"])
     # Assign the functions from each file to the corresponding tab
     with tab1:
         show_anomaly()
@@ -24,16 +24,16 @@ def main():
         show_compare()
 
     with tab3:
-        show_costcalculator()
-
-    with tab4:
         show_savingsplan()
 
-    with tab5:
+    with tab4:
         show_solar()
 
-    with tab6:
+    with tab5:
         show_wind()
+
+    with tab6:
+        show_costcalculator()
 
 if __name__ == "__main__":
     main()
